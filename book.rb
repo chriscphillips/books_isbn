6a5d50ba9d49d70883d61370c6032828f0579cb2
chriscphillips <cptp50wv@shentel.net>
2018-01-17 15:20:47 -0500

    class project

--- book.rb
+++ book.rb
@@ -1,1 +1,11 @@
- 
+ def isbn(book)
+    isbn_hash = {"9780999355107" => "Cut Throat Syndrome", "9781501175466" => "IT", "9780399575570" => "Typhoon Fury", 
+    	"9780399183973" => "Pirate", "9780399185304" => "The Midnight Bell", "0399148337" => "Midnights Runner"}
+isbn_hash[book]
+end
+
+def isbn_photo(book)
+	 isbn_hash_photo = {"9780999355107" => "img//cutthroatsyndrome.jpg", "9781501175466" => "img//it.jpg", "9780399575570" => "img//typhoonfury.jpg", 
+    	"9780399183973" => "img//pirate.jpg", "9780399185304" => "img//themidnightbell.jpg", "0399148337" => "img//midnightrunner.jpg"}
+ isbn_hash_photo[book]   	
+end
--- testbook.rb
+++ testbook.rb
@@ -1,2 +1,14 @@
+require "minitest/autorun"
+require_relative "book.rb"
 
+class TestBook < Minitest::Test 
+
+	def test_assert_that_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_assert_that_9780999355107_returns_book
+		assert_equal("Cut Throat Syndrome", isbn("9780999355107"))
+	end
+end
 	
